5488ba0c5b5f4fbce974d9fc983e86e5ef9e4135
LukeU1 <luunter14@gmail.com>
2022-06-20 10:33:17 -0500

    add test file

---
+++ test4234234.py
@@ -0,0 +1,7 @@
+import pandas as pd
+#df = pd.read_csv('https://github.com/slimegg1/Test.git')
+#df = pd.read_csv('/home/pi/luke.csv')
+df = pd.read_csv('/home/pi/Test/tested.csv')
+
+print (df)
+df.head()
